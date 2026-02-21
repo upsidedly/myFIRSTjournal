b6a8cd98e3bdbf0a203d5493f71f5f9c5b8c7702
Amari Cross <amaricross111@gmail.com>
2026-02-21 07:20:31 -0500

    feat: update footer copyright and team name

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -2,7 +2,7 @@ const Footer = () => {
   return (
     <footer className="fixed bottom-0 w-full py-2.5 text-center">
       <p className="text-muted-foreground/60 text-sm">
-        Matthew Williams &copy; 2025 
+        Crimson Crocs Team 16075 &copy; 2026 
         <span className="mx-2.5">&bull;</span> 
         Break some furniture
       </p>
